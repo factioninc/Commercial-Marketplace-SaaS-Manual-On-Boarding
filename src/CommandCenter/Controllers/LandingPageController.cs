ce061f93ce8763aeb12440ed61c86f051b5b4319
GPeters-Faction <graeme.peters@factioninc.com>
2021-06-08 11:17:40 -0600

    Reinstate commented lines for actual functionality.

--- src/CommandCenter/Controllers/LandingPageController.cs
+++ src/CommandCenter/Controllers/LandingPageController.cs
@@ -68,7 +68,6 @@ namespace CommandCenter.Controllers
         /// <returns>Action result.</returns>
         public async Task<ActionResult> Index(string token, CancellationToken cancellationToken)
         {
-            /*
             if (string.IsNullOrEmpty(token))
             {
                 this.ModelState.AddModelError(string.Empty, "Token URL parameter cannot be empty");
@@ -138,12 +137,11 @@ namespace CommandCenter.Controllers
 
             this.ModelState.AddModelError(string.Empty, "Cannot resolve subscription");
             return this.View();
-             */
 
             // This is just for testing or Demo purposes
-            var model = new AzureSubscriptionProvisionModel();
-            model.CustomBundleOptions = new FactionCustomBundleModel();
-            return this.View(model);
+            // var model = new AzureSubscriptionProvisionModel();
+            // model.CustomBundleOptions = new FactionCustomBundleModel();
+            // return this.View(model);
         }
 
         /// <summary>
